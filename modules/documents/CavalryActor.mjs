6100a6979b7a115b2aaad1a0d37fa1388d25a4c2
Hay4 Hay4enko <haboji04.ttapllluba@gmail.com>
2024-06-27 20:32:52 +0300

    New sheets test

--- modules/documents/CavalryActor.mjs
+++ modules/documents/CavalryActor.mjs
@@ -18,4 +18,53 @@ export class CavalryFVTTActor extends Actor {
 
         const systemData = actorData.system;
     }
+
+    activateListeners(html) {
+        super.activateListeners(html);
+      
+        // Render the item sheet for viewing/editing prior to the editable check.
+        html.on('click', '.item-edit', (ev) => {
+          const li = $(ev.currentTarget).parents('.item');
+          const item = this.actor.items.get(li.data('itemId'));
+          item.sheet.render(true);
+        });
+      
+        // -------------------------------------------------------------
+        // Everything below here is only needed if the sheet is editable
+        if (!this.isEditable) return;
+      
+        // Add Inventory Item
+        html.on('click', '.item-create', this._onItemCreate.bind(this));
+      
+        // Delete Inventory Item
+        html.on('click', '.item-delete', (ev) => {
+          const li = $(ev.currentTarget).parents('.item');
+          const item = this.actor.items.get(li.data('itemId'));
+          item.delete();
+          li.slideUp(200, () => this.render(false));
+        });
+      
+        // Active Effect management
+        html.on('click', '.effect-control', (ev) => {
+          const row = ev.currentTarget.closest('li');
+          const document =
+            row.dataset.parentId === this.actor.id
+              ? this.actor
+              : this.actor.items.get(row.dataset.parentId);
+          onManageActiveEffect(ev, document);
+        });
+      
+        // Rollable abilities.
+        html.on('click', '.rollable', this._onRoll.bind(this));
+      
+        // Drag events for macros.
+        if (this.actor.isOwner) {
+          let handler = (ev) => this._onDragStart(ev);
+          html.find('li.item').each((i, li) => {
+            if (li.classList.contains('inventory-header')) return;
+            li.setAttribute('draggable', true);
+            li.addEventListener('dragstart', handler, false);
+          });
+        }
+      }      
 }
